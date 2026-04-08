fae599ded103f2587d04db3b70691b4a3df0e61f
Тиаментий Лёвин <tiamenti@gmail.com>
2026-04-08 16:16:42 +0000

    city event factory

--- app/Models/CityEvent.php
+++ app/Models/CityEvent.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use App\Enums\CityEventStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CityEvent extends Model
 {
+    use HasFactory;
+
     protected $casts = [
         'start_at' => 'datetime',
         'end_at' => 'datetime',
---
+++ database/factories/CityEventFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\CityEventStatus;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CityEventFactory extends Factory
+{
+    public function definition(): array
+    {
+        $startAt = fake()->dateTimeBetween('now', '+30 days');
+        $endAt = (clone $startAt)->modify('+8 hours');
+
+        return [
+            'title' => fake()->sentence(nbWords: 4),
+            'place' => fake()->city().', '.fake()->streetName(),
+            'start_at' => $startAt,
+            'end_at' => fake()->dateTimeBetween($startAt, $endAt),
+            'tags' => fake()->words(4),
+            'capacity' => fake()->numberBetween(1, 5000),
+            'status' => fake()->randomElement(CityEventStatus::cases()),
+            'popularity' => fake()->numberBetween(1, 5),
+            'change_number' => fake()->numberBetween(1, 10),
+        ];
+    }
+}
