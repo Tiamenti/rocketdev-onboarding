4cb95cd909f9998307aafa378868fa01e3d110e1
Тиаментий Лёвин <tiamenti@gmail.com>
2026-04-08 16:40:39 +0000

    calculate popularity method

--- app/Models/CityEvent.php
+++ app/Models/CityEvent.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use App\Enums\CityEventStatus;
+use App\Observers\CityEventObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Number;
 
+#[ObservedBy(CityEventObserver::class)]
 class CityEvent extends Model
 {
     use HasFactory;
@@ -16,4 +20,15 @@ class CityEvent extends Model
         'tags' => 'array',
         'status' => CityEventStatus::class,
     ];
+
+    public function calculatePopularity(): int
+    {
+        $daysToStart = today()->diffInDays($this->start_at);
+
+        $raw = 3 + ($this->capacity / 1000) - ($daysToStart / 10) + count($this->tags);
+
+        $popularity = round($raw);
+
+        return Number::clamp($popularity, 1, 5);
+    }
 }
