c33484a3308c1d3c0f88b6f09fd1d13f51a34c85
Тиаментий Лёвин <tiamenti@gmail.com>
2026-04-08 18:42:22 +0000

    event change number

--- app/Observers/CityEventObserver.php
+++ app/Observers/CityEventObserver.php
@@ -6,6 +6,11 @@ use App\Models\CityEvent;
 
 class CityEventObserver
 {
+    public function updating(CityEvent $cityEvent): void
+    {
+        $cityEvent->change_number += 1;
+    }
+
     public function saving(CityEvent $cityEvent): void
     {
         $cityEvent->popularity = $cityEvent->calculatePopularity();
